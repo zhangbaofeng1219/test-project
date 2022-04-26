f910ebdc6697fb2df581895c7825a298ef618c6c
大唐响马 <zhangbaofeng1022@163.com>
2022-04-27 01:07:43 +0800

    Update run-bk_install.sh

--- run-bk_install.sh
+++ run-bk_install.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-domain=${1}
-bk_psd=${2}
-#自定义域名、安装目录以及登陆密码
-# 执行前请使用实际的顶级域名 (如：bktencent.com) 和安装目录进行替换
 
-cd /data/install  && ./configure -d ${domain} -p /data/bkce
+echo "###########################################【安装 START】################################################"
 
-cat > /data/install/bin/03-userdef/usermgr.env << EOF
-BK_PAAS_ADMIN_PASSWORD=${bk_psd}
-EOF
+bash /data/install/configure_ssh_without_pass
 
-### 开始安装
 cd /data/install
 cd /data/install  && yes yes|./bk_install common
 cd /data/install  && ./health_check/check_bk_controller.sh
@@ -29,3 +22,5 @@ cd /data/install  && ./bkcli initdata topo
 cd /data/install/
 cd /data/install  && echo bkssm bkiam usermgr paas cmdb gse job consul | xargs -n 1 ./bkcli check
 
+
+echo "###########################################【安装 END】################################################"
